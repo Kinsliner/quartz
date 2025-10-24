2a6705c3769a002fc3a22efd315d6c5a00221789
echen <echen.wan9@gmail.com>
2025-10-24 10:11:57 +0800

    Quartz sync: Oct 24, 2025, 10:11 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "www.easondev.dpdns.org",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
